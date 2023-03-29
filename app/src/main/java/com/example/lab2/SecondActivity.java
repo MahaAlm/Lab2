47fd9df29f3d449ce7e59e0edbe377596c601465
Maha Almuaythir <m.almuaythir2@gmail.com>
2023-03-29 11:15:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab2/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView receiver_msg;
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        receiver_msg=(TextView) findViewById(R.id.received_value_id);
+        receiver_msg.setText("Hello "+str+"!");
+
+
+    }
+}
